ced47f500610ad9523c20f1e3ffeb4fac4e42cc8
javenkn <javenkn7@yahoo.com>
2016-07-02 12:05:53 -1000

    works with both curl -I and curl. Also the browser works with the styles.css

--- server.js
+++ server.js
@@ -3,24 +3,23 @@ var fs = require('fs');
 
 var CONFIG = require('./config');
 
-var htmlFiles = [];
-var args = process.argv;
-
 var server = net.createServer(function (socket) { //readable socket
   // connection listener
   console.log('Somebody connected!');
   socket.on('data', (data) => {
     var dateNow = new Date();
     var UTCDate = dateNow.toUTCString();
-    console.log(args);
 
     var requestMethod = data.toString().split('\n')[0];
+    console.log(requestMethod);
+    var httpVers = requestMethod.slice(-9).trim();
     var path = requestMethod.split(' ')[1].slice(1);
     if(path === ''){
       path = 'index.html';
     }
     fs.exists(path, function (exists) {
       if (exists) {
+        var statusLine = ' 200 OK';
         fs.stat(path, function (error, stats) {
           if (error) throw error;
             fs.open(path, 'r', function (error, fileData) {
@@ -28,22 +27,45 @@ var server = net.createServer(function (socket) { //readable socket
               var buffer = new Buffer(stats.size);
               fs.read(fileData, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
                 if(error) throw error;
-                var htmlData = buffer.toString('utf8', 0, buffer.length).replace('\t','');
-                socket.write('\n' + requestMethod.slice(6) + '\n' +
+                var htmlData = buffer.toString('utf8', 0, buffer.length);
+                socket.write(httpVers + statusLine  + '\n' +
                                  'Date: ' + UTCDate + '\n' +
                                  'Server: localhost' + '\n' +
                                  'Content-Length: ' + bytesRead + '\n\n' +
-                                 buffer + '\n'
+                                 htmlData
                                  );
 
+                socket.end();
               });
             });
         });
       }else{
-        socket.write('Path does not exist.\n');
+        var errorStatusLine = ' 404 Not Found';
+        path = '404.html';
+        fs.stat(path, function (error, stats) {
+          if (error) throw error;
+            fs.open(path, 'r', function (error, fileData) {
+              if(error) throw error;
+              var buffer = new Buffer(stats.size);
+              fs.read(fileData, buffer, 0, buffer.length, null, function (error, bytesRead, buffer) {
+                if(error) throw error;
+                var htmlData = buffer.toString('utf8', 0, buffer.length);
+                socket.write(httpVers + errorStatusLine  + '\n' +
+                                 'Date: ' + UTCDate + '\n' +
+                                 'Server: localhost' + '\n' +
+                                 'Content-Length: ' + bytesRead + '\n\n' +
+                                 htmlData
+                                 );
+
+
+                socket.end();
+              });
+            });
+        });
       }
     });
   });
+
 });
 
 server.listen(CONFIG.PORT, () => {
